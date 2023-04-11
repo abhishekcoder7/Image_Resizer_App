0c93769b32c454a74c1e7997c2999b43105c8e1b
Abhishek Singh <mukundsinghg@gmail.com>
2023-04-12 00:35:42 +0530

    Output Image Ready

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import cv2
 
+
+scale = int(input("Enter Scaling percentage : "))
+
+img = cv2.imread('source_img.jpg', cv2.IMREAD_UNCHANGED)
+
+new_height = int(img.shape[0] * scale / 100)
+new_width = int(img.shape[1] * scale / 100)
+
+output_img = cv2.resize(img, (new_width, new_height))
+
+cv2.imwrite('output.jpg', output_img)
